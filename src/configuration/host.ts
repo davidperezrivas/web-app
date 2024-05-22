66e9e068269e8016ff0e5d24d31f02df248b9a93
David Perez <davip.rivas@gmail.com>
2024-05-22 18:51:24 -0400

    feat create a variable to get the domain to environment

--- src/configuration/host.ts
+++ src/configuration/host.ts
@@ -1,5 +1,5 @@
 const domain = window.location.origin;
 
-const host = domain.indexOf('localhost') >= 0 ? 'http://localhost:3001' : 'https://users-mubx.onrender.com/';
+const host = domain.indexOf('localhost') >= 0 ? 'http://localhost:3001' : 'https://users-mubx.onrender.com';
 
 export default host;

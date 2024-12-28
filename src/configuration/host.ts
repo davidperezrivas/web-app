022a112cf0f2b4ecb68e3a95f01a150349bb18ef
David Perez <davip.rivas@gmail.com>
2024-12-27 23:01:19 -0300

    fix url to host with production

--- src/configuration/host.ts
+++ src/configuration/host.ts
@@ -1,5 +1,5 @@
 const domain = window.location.origin;
 
-const host = domain.indexOf('localhost') >= 0 ? 'http://localhost:3001' : 'http://72.167.50.134:3001/';
+const host = domain.indexOf('localhost') >= 0 ? 'http://localhost:3001' : 'http://72.167.50.134:3001';
 
 export default host;

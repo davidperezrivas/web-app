eff4d4cd6a2135374c826c67778b50c8ddf34a32
David Perez <davip.rivas@gmail.com>
2024-12-27 22:58:31 -0300

    fix url to host with production

--- src/configuration/host.ts
+++ src/configuration/host.ts
@@ -1,5 +1,5 @@
 const domain = window.location.origin;
 
-const host = domain.indexOf('localhost') >= 0 ? 'http://localhost:3001' : 'https://bff-kkt4.onrender.com';
+const host = domain.indexOf('localhost') >= 0 ? 'http://localhost:3001' : 'http://72.167.50.134:3001/';
 
 export default host;
